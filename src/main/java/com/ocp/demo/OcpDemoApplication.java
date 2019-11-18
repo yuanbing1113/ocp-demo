05ffe49d3ac03b60648df76e72cf9c9ceefd1ea6
yuanbing1113 <yuanbing1113@gmail.com>
2019-11-18 10:21:24 +0800

    first commit

---
+++ src/main/java/com/ocp/demo/OcpDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ocp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcpDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcpDemoApplication.class, args);
+	}
+
+}
